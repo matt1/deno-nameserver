8a965efb46889d0b35fc088e981ee5d8f90d272a
matt1 <matt1@users.noreply.github.com>
2020-08-25 16:04:59 +0100

    IPv6 address normalisation + tests.

--- utils.ts
+++ utils.ts
@@ -13,3 +13,24 @@ export function numberToIpv4(ipv4:string):number {
   result += Number(bytes[3]);
   return result;
 }
+
+/** 
+ * Normalise a IPv6 address so that `::` are replaced with zeros.
+ */
+export function normaliseIpv6(ipv6:string):string {
+  if (ipv6.indexOf('::') < 0) return ipv6;
+
+  // Use :: as the pivot and split into left and right.
+  const parts = ipv6.split('::');
+  const leftParts = parts[0].length === 0 ? [] : parts[0].split(':');
+  const rightParts = parts[1].length === 0 ? [] : parts[1].split(':');
+
+  // Work out how many zero-groups were collapsed in :: by counting length of
+  // the left and right parts, then create the missing groups.
+  const missingZeros = 8 - (leftParts.length + rightParts.length);
+  const newZeros = [];
+  for (let i = 0; i < missingZeros; i++) {
+    newZeros.push('0');
+  }
+  return [...leftParts, ...newZeros, ...rightParts].join(':');
+}
--- utils_test.ts
+++ utils_test.ts
@@ -1,5 +1,5 @@
-import { assertEquals} from "https://deno.land/std/testing/asserts.ts";
-import { hex, numberToIpv4 } from "./utils.ts";
+import { assertEquals, assert} from "https://deno.land/std/testing/asserts.ts";
+import { hex, numberToIpv4, normaliseIpv6 } from "./utils.ts";
 
 Deno.test('Utils converts IPv4 addresses from strings to ints', () => {
   assertEquals(numberToIpv4('127.0.0.1'), 2130706433);
@@ -10,4 +10,12 @@ Deno.test('Utils converts IPv4 addresses from strings to ints', () => {
 Deno.test('Utils converts numbers to hex', () => {
   assertEquals(hex(0), '0x0');
   assertEquals(hex(100), '0x64');
+});
+
+Deno.test('Utils normalises IPv6', () => {
+  assertEquals(normaliseIpv6('0:0:0:0:0:0:0:1'), '0:0:0:0:0:0:0:1');
+  assertEquals(normaliseIpv6('::1'), '0:0:0:0:0:0:0:1');
+  assertEquals(normaliseIpv6('1::'), '1:0:0:0:0:0:0:0');
+  assertEquals(normaliseIpv6('2001:db8::2:1'), '2001:db8:0:0:0:0:2:1');
+  assertEquals(normaliseIpv6('2001:db8:0:1:1:1:1:1'), '2001:db8:0:1:1:1:1:1');
 });
